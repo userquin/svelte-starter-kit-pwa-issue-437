2875b01d79bce879fdc66eddf5f07e3f64f57225
Sumanth Chinthagunta <xmlking@gmail.com>
2022-11-27 14:51:55 -0800

    fix(policy): add client side validation along with form actions

--- src/routes/(app)/dashboard/policies/[id]/+page.svelte
+++ src/routes/(app)/dashboard/policies/[id]/+page.svelte
@@ -287,9 +287,9 @@
 </form>
 
 <!-- debug -->
-<pre class="p-4">$isValid: {$isValid}</pre>
+<!-- <pre class="p-4">$isValid: {$isValid}</pre>
 <pre class="p-4">$fData: {JSON.stringify($fData, null, 4)}</pre>
-<pre class="p-4">$fErrors: {JSON.stringify($fErrors, null, 4)}</pre>
+<pre class="p-4">$fErrors: {JSON.stringify($fErrors, null, 4)}</pre> -->
 
 <style lang="postcss">
 	:global(.sv-control) {
